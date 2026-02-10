eaefe7895c3d7aaa49896d389512d990ba45ef7e
Midhun-Nk <midhunnk2021@gmail.com>
2026-02-10 20:12:08 +0530

    Updated V3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+  output: "export", // ✅ REQUIRED for Render Static Site
 
   images: {
+    unoptimized: true, // ✅ REQUIRED for static export
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "www.google.com" },
